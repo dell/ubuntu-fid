d9fd0cfff33b421090f67cbc294715de782c0855
Mario Limonciello <Mario_Limonciello@Dell.com>
2008-07-17 15:37:06 -0500

    add flash 9 from Ubuntu 8.04

---
+++ framework/scripts/chroot-scripts/30-run_flashplayer_installer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+. /mnt/scripts/chroot-scripts/fifuncs ""
+
+IFHALT "Run flashplayer-installer..."
+
+. /mnt/scripts/chroot-scripts/flashplayer-installer
+
+IFHALT "Done with  flashplayer-installer..."

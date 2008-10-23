72a451187bcbb5f6594faf4635543951ea79af9b
Veronica Camp <root@localhost.localdomain>
2008-10-23 16:17:34 -0500

    Removing 30-run_flashplayer_installer.sh script

--- framework/scripts/chroot-scripts/os-post/30-run_flashplayer_installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. /cdrom/scripts/chroot-scripts/fifuncs ""
-
-IFHALT "Run flashplayer-installer..."
-
-. /cdrom/misc/flash/flashplayer-installer
-
-IFHALT "Done with  flashplayer-installer..."

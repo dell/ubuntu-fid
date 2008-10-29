deae24d43a200efb457566facc94e004d7d72923
Mario Limonciello <Mario_Limonciello@Dell.com>
2008-10-29 12:04:55 -0500

    fix typographical error in 01-graphics

--- framework/scripts/chroot-scripts/fish/01-graphics.sh
+++ framework/scripts/chroot-scripts/fish/01-graphics.sh
@@ -41,7 +41,6 @@ elif `lspci | grep VGA | grep 'AMD\|ATI' >/dev/null`; then
 		gdebi -n /cdrom/debs/fglrx/*kernel-source*.deb
 		gdebi -n /cdrom/debs/fglrx/xorg-driver-fglrx*.deb
 
-	<---- The AMD DVD check is turned off until the driver is functional ---->
 	#We don't have newer drivers, lets install from the DVD
 	else
 		apt-get install xorg-driver-fglrx -y

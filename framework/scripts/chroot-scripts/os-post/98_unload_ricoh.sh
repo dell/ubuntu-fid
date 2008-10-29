731eaaa92e55ae501112a796d63fb3b631d6c9a1
Mario Limonciello <Mario_Limonciello@Dell.com>
2008-10-29 12:05:46 -0500

    add a header to 98_unload_ricoh

--- framework/scripts/chroot-scripts/os-post/98_unload_ricoh.sh
+++ framework/scripts/chroot-scripts/os-post/98_unload_ricoh.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
+#
+#       <98_unload_ricoh.sh>
+#
+#       Remove the ricoh_mmc driver if it's loaded.
+#       It can cause major problems in MDIAGS since the
+#       PCI address space has changed if it is still loaded
+#       when rebooting.
+#
+#       Copyright 2008 Dell Inc.
+#           Mario Limonciello <Mario_Limonciello@Dell.com>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
 
-#Remove the ricoh_mmc driver if it's loaded.
-#It can cause major problems in MDIAGS since the
-#PCI address space has changed if it is still loaded
-#when rebooting.
+. /cdrom/scripts/chroot-scripts/fifuncs ""
+
+IFHALT "Unload ricoh_mmc if it was loaded..."
 
 if lsmod | grep ricoh_mmc >/dev/null; then
 	rmmod ricoh_mmc

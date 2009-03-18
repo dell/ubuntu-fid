54da0051c1006544a43d64beaf0eb2bab04d2a8e
Mario Limonciello <Mario_Limonciello@Dell.com>
2009-03-18 11:57:04 -0500

    05-audio-levels.sh shouldn't ever cause the FI to fail in case audio isn't supported on this platform

--- framework/scripts/chroot-scripts/fish/05-audio-levels.sh
+++ framework/scripts/chroot-scripts/fish/05-audio-levels.sh
@@ -26,4 +26,4 @@
 amixer -c 0 -q set Front 100% unmute 2>&1 || true
 amixer -c 0 -q set PCM 100% unmute 2>&1 || true
 amixer -c 0 -q set Master 80% unmute 2>&1 || true
-alsactl store 0 2>&1
+alsactl store 0 2>&1 || true

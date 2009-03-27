581cac37f90b45bf95b2896535fb46ed7612d3bc
Mario Limonciello <Mario_Limonciello@Dell.com>
2009-03-27 18:32:56 -0500

    install grub-pc from the DVD for china since it's now found there

--- china-framework/scripts/chroot-scripts/os-pre/70-activate-grub2.sh
+++ china-framework/scripts/chroot-scripts/os-pre/70-activate-grub2.sh
@@ -2,22 +2,19 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# Install debs from debs/grub2
-if ls /cdrom/debs/grub2/*.deb > /dev/null 2>&1; then
-    #to prevent postinst scripts complaining
-    #in noninteractive mode, take out grub1
-    #first
-    rm /boot/grub -rf
-    gdebi -n /cdrom/debs/grub2/*.deb
+#to prevent postinst scripts complaining
+#in noninteractive mode, take out grub1
+#first
+rm /boot/grub -rf
+apt-get install -y grub-pc
 
-    #Reinstall grub2 to set the default bootloader to 
-    #be grub2 rather than chainloading
-    grub-install '(hd0,3)'
+#Reinstall grub2 to set the default bootloader to 
+#be grub2 rather than chainloading
+grub-install '(hd0,3)'
 
-    #don't generate grub.cfg now.  it will be generated
-    #after the artwork is installed
-    touch /boot/grub/grub.cfg
+#don't generate grub.cfg now.  it will be generated
+#after the artwork is installed
+touch /boot/grub/grub.cfg
 
-    rm -f /etc/kernel/prerm.d/last-good-boot
-fi
+rm -f /etc/kernel/prerm.d/last-good-boot
 

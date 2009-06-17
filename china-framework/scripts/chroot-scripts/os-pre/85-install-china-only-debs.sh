92f7da14801abaa56092a49caeb1d46ae74b9967
Mario Limonciello <Mario_Limonciello@Dell.com>
2009-06-17 17:55:46 -0500

    install china customizations on their own to ensure the recovery message shows up.  something was going wrong by installing dell-recovery and dell-china-customizations at the same time

---
+++ china-framework/scripts/chroot-scripts/os-pre/85-install-china-only-debs.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+#       <85-install-china-only-debs.sh>
+#
+#       Installs any debian archives (.deb) that aren't in the base install
+#       and only used in China.
+#
+#       Copyright 2008-2009 Dell Inc.
+#           Mario Limonciello <Mario_Limonciello@Dell.com>
+#           Hatim Amro <Hatim_Amro@Dell.com>
+#           Michael E Brown <Michael_E_Brown@Dell.com>
+#
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+. /cdrom/scripts/chroot-scripts/fifuncs ""
+
+IFHALT "Installing debs from debs/china"
+
+if ls /cdrom/debs/china/*.deb > /dev/null 2>&1; then
+    dpkg -i /cdrom/debs/china/*.deb
+fi
+

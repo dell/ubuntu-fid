d138de7513505b22e13133b04a54097d8d02cda2
Mario Limonciello <Mario_Limonciello@Dell.com>
2009-07-16 11:08:58 -0500

    Canonical is mounting the squashfs at /root for us again, so we can revert back to old scripts. Revert "use anna-install to get us parted, and comment out the grub to rp install for now"
    This reverts commit 5b0232e523b5e88ca92d0dd7007aa40a18a8bb71.

--- framework/scripts/bootstrap/10-format.sh
+++ framework/scripts/bootstrap/10-format.sh
@@ -26,20 +26,22 @@
 #       MA 02110-1301, USA.
 
 if ! grep -q INTERACTIVE /proc/cmdline; then
-
-    anna-install parted-udeb
+    # /root/dev and /dev need to be in sync while we do this file
+    mount -o bind /dev /root/dev
 
     # Mark RP bootable
-    sfdisk -A${RP_PART_NUM} ${BOOTDEV}
+    chroot /root sfdisk -A${RP_PART_NUM} ${BOOTDEV}
 
     # * Delete partitions 3 & 4 since that's where we are installing to
-    parted -s ${BOOTDEV} rm 3 || :
-    parted -s ${BOOTDEV} rm 4 || :
+    chroot /root/ parted -s ${BOOTDEV} rm 3 || :
+    chroot /root/ parted -s ${BOOTDEV} rm 4 || :
 
-#    # Install grub onto the RP
-#    chroot /root grub <<-EOF
-#root (hd0,1)
-#setup (hd0,1)
-#quit
-#EOF
+    # Install grub onto the RP
+    chroot /root grub <<-EOF
+root (hd0,1)
+setup (hd0,1)
+quit
+EOF
+
+    umount /root/dev
 fi

bd1252c7fcfbfec2a7d2f40b36b8096f375225a6
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-02-23 17:32:55 -0600

    Fix inverted logic on active partition setting check at end of install

--- framework/scripts/chroot-scripts/os-post/99-active_partition.sh
+++ framework/scripts/chroot-scripts/os-post/99-active_partition.sh
@@ -28,7 +28,7 @@
 
 . /cdrom/scripts/chroot-scripts/fifuncs ""
 
-if [ "$BOOTDEV" != "$TARGETDEV" ]; then
+if [ "$BOOTDEV" = "$TARGETDEV" ]; then
     IFHALT "Activate bootable partition..."
 
     #Sets the active partition to the newly installed partition

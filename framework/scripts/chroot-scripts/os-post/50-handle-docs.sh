af569087962530ce38054b7f29cc69c661916850
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-04-10 14:33:14 -0500

    Use postinstall scripts that come from dell-recovery.  This requires you to use dell-recovery 0.53 or newer. The SUCCESS and FAIL scripts will be removed after the first installation (as they are no longer necessary).
    It also means less loose files sitting around and needing to be pulled in via the FID tree

--- framework/scripts/chroot-scripts/os-post/50-handle-docs.sh
+++ framework/scripts/chroot-scripts/os-post/50-handle-docs.sh
@@ -24,7 +24,7 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-. /cdrom/scripts/chroot-scripts/fifuncs ""
+. /usr/share/dell/scripts/fifuncs ""
 
 IFHALT "Copying Vendor Documents..."
 

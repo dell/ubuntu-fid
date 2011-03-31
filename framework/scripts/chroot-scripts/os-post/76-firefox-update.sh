9f7ed874dfc2ea8900e6926b229c90886d3ea2c7
Mario Limonciello <Mario_Limonciello@Dell.com>
2011-03-31 11:50:09 -0500

    add updated firefox packages to fix yahoo problems

---
+++ framework/scripts/chroot-scripts/os-post/76-firefox-update.sh
@@ -0,0 +1,3 @@
+. /usr/share/dell/scripts/fifuncs
+IFHALT "Installing updated firefox packages..."
+apt-get install --yes --allow-unauthenticated brasero firefox firefox-gnome-support firefox-branding
